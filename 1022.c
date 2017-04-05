8b12e4ac493ee346c7f2c76fadb0a4f124c34a68
Ridineu de Moraes Neto <ridineu@hotmail.com>
2017-04-05 00:13:26 -0300

    Accepted

--- 1022.c
+++ 1022.c
@@ -15,11 +15,11 @@ TReal Simplificacao(TReal);
 int mdc(int, int);
 
 int main(){
-	
+
 	TReal f1, f2;
 	char sinal;
 	int n;
-	
+
 	scanf("%d", &n);
 	while(n--){
 		scanf("%d / %d %c %d / %d", &f1.numerador, &f1.denominador, &sinal, &f2.numerador, &f2.denominador);
@@ -34,8 +34,8 @@ int main(){
 				else
 					f1 = Multiplicao(f1, f2);
 		f2 = Simplificacao(f1);
-		printf("%d/%d = %d/%d\n", f1.numerador, f1.denominador, f2.numerador, f2.denominador);			
-	}		
+		printf("%d/%d = %d/%d\n", f1.numerador, f1.denominador, f2.numerador, f2.denominador);
+	}
 	return 0;
 }
 
@@ -44,8 +44,8 @@ TReal Adicao(TReal i, TReal j){
 	/* Soma: (N1*D2 + N2*D1) / (D1*D2) */
 	r.numerador = i.numerador * j.denominador + j.numerador * i.denominador;
 	r.denominador = i.denominador * j.denominador;
-	
-	return r;	
+
+	return r;
 }
 
 TReal Subtracao(TReal i, TReal j){
@@ -53,7 +53,7 @@ TReal Subtracao(TReal i, TReal j){
 	/* Subtração: (N1*D2 - N2*D1) / (D1*D2) */
 	r.numerador = i.numerador * j.denominador - j.numerador * i.denominador;
 	r.denominador = i.denominador * j.denominador;
-	
+
 	return r;
 }
 
@@ -62,8 +62,8 @@ TReal Multiplicao(TReal i, TReal j){
 	/* Multiplicação: (N1*N2) / (D1*D2) */
 	r.numerador = i.numerador * j.numerador;
 	r.denominador = i.denominador * j.denominador;
-	
-	return r; 
+
+	return r;
 }
 
 TReal Divisao(TReal i, TReal j){
@@ -71,29 +71,27 @@ TReal Divisao(TReal i, TReal j){
 	/* Divisão: (N1/D1) / (N2/D2), ou seja (N1*D2)/(N2*D1) */
 	r.numerador = i.numerador * j.denominador;
 	r.denominador = j.numerador * i.denominador;
-	
+
 	return r;
 }
 
 TReal Simplificacao(TReal i){
 	int d;
-	
+
 	d = mdc(i.numerador, i.denominador);
 	i.numerador = i.numerador / d;
 	i.denominador = i.denominador / d;
-	
+
 	return i;
 }
 
 int mdc(int i, int j){
-	if(i < 0)	
+	if(i < 0)
 		i = -i;
-	if(j < 0)	
+	if(j < 0)
 		j = -j;
 	if(i % j == 0)
         return j;
     else
         return mdc(j, i % j);
 }
-
-
